e99604fcb4eb40bbb68972c8cb6a664caafaca25
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2013-05-18 12:28:40 +0200

    Use existing feed to filter out previous articles.

--- hn-digest.js
+++ hn-digest.js
@@ -1,75 +1,99 @@
-var argv, RSS, _, request, cheerio, rss, moment, fs, feed, tmpl;
+var argv, fs, cheerio, RSS, _, request, rss, moment, feed, tmpl;
 
 argv = require("optimist").usage("Usage: $0")
                           .demand("p")
                           .alias("p", "path")
                           .describe("p", "Output file")
                           .argv;
+fs = require("fs");
+cheerio = require("cheerio");
 RSS = require("rss");
 _ = require("underscore");
 request = require("request");
-cheerio = require("cheerio");
 moment = require("moment");
-fs = require("fs");
 
-feed = new RSS({
-    title: "HN Digest",
-    description: "Digest of the top HN articles.",
-    site_url: "https://news.ycombinator.com"
-});
+fs.readFile(argv.p, function (err, data) {
 
-tmpl = _.template('<li><a href="<%= link %>"><%= title %></a> &mdash; <a href="https://news.ycombinator.com/item?id=<%= id %>">Comments</a></li>');
+    var previousArticles, $previousFeed, $previousDigest;
 
-request("https://news.ycombinator.com/", function (err, resp, body) {
+    previousArticles = [];
 
-    var $, articles, description, date;
-
-    $ = cheerio.load(body);
-
-    articles = [];
-
-    description = "<ol>";
-
-    $("td.title a").each(function (idx, el) {
-        var $headline, $comments, idPattern;
-        $headline = $(el);
-        $comments = $("a", $headline.parent().parent().next()).last();
-     // Ignore `More` button.
-        if ($headline.attr("href") === "news2") {
-            return;
-        }
-     // Ignore ads (articles without comments).
-        if ($comments.length === 0) {
-            return;
-        }
-        idPattern = /item\?id=(\d+)/;
-        articles.push({
-            id: idPattern.exec($comments.attr("href"))[1],
-            title: $headline.text(),
-            link: $headline.attr("href")
+    // Try to open existing feed to get a list of previous articles.
+    if (err === null) {
+        $previousFeed = cheerio.load(data);
+        $previousDigest = cheerio.load($previousFeed("item description").text());
+        $previousDigest("li a.article").each(function (idx, el) {
+            var $article;
+            $article = $previousDigest(el);
+            previousArticles.push($article.attr("href"));
         });
-    });
+    }
 
-    articles.forEach(function (article) {
-        description += tmpl(article);
-    });
-
-    description += "</ol>";
-
-    date = moment();
-
-    feed.item({
+    feed = new RSS({
         title: "HN Digest",
-        description: description,
-        guid: date.format("YYYY-MM-DD ha")
+        description: "Digest of the top HN articles.",
+        site_url: "https://news.ycombinator.com"
     });
 
-    fs.writeFile(argv.p, feed.xml(), function (err) {
-        if (err !== null) {
-            console.error("Feed could not be written to disk:", err);
-        } else {
-            console.log("Feed was written to disk.");
-        }
+    tmpl = _.template('<li><a href="<%= link %>" class="article"><%= title %></a> &mdash; <a href="https://news.ycombinator.com/item?id=<%= id %>">Comments</a></li>');
+
+    request("https://news.ycombinator.com/", function (err, resp, body) {
+
+        var $, articles, description, date;
+
+        $ = cheerio.load(body);
+
+        articles = [];
+
+        description = "<ol>";
+
+        $("td.title a").each(function (idx, el) {
+            var $headline, $comments, idPattern;
+            $headline = $(el);
+            $comments = $("a", $headline.parent().parent().next()).last();
+         // Ignore `More` button.
+            if ($headline.attr("href") === "news2") {
+                return;
+            }
+         // Ignore ads (articles without comments).
+            if ($comments.length === 0) {
+                return;
+            }
+            idPattern = /item\?id=(\d+)/;
+            articles.push({
+                id: idPattern.exec($comments.attr("href"))[1],
+                title: $headline.text(),
+                link: $headline.attr("href")
+            });
+        });
+
+        // Ignore old articles.
+        articles = articles.filter(function (article) {
+            return previousArticles.indexOf(article.link) == -1;
+        });
+
+        articles.forEach(function (article) {
+            description += tmpl(article);
+        });
+
+        description += "</ol>";
+
+        date = moment();
+
+        feed.item({
+            title: "HN Digest",
+            description: description,
+            guid: date.format("YYYY-MM-DD ha")
+        });
+
+        fs.writeFile(argv.p, feed.xml(), function (err) {
+            if (err !== null) {
+                console.error("Feed could not be written to disk:", err);
+            } else {
+                console.log("Feed was written to disk.");
+            }
+        });
+
     });
 
 });
